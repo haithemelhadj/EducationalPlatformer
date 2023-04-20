55bace94d3be0a192d89e1257909044ad024a031
chourouk07 <117457141+chourouk07@users.noreply.github.com>
2023-04-20 13:09:49 +0200

    commit

--- Assets/Scripts/Spelling.cs
+++ Assets/Scripts/Spelling.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Reflection;
 
 public class Spelling : MonoBehaviour
 {
@@ -8,15 +11,25 @@ public class Spelling : MonoBehaviour
     public Sprite[] objects; 
     public int randomIndex;
     public string word;
-    
+    public TMP_Text displayText;
+    public GameObject displaySprite; 
+    string emptyWord = "";
+
     void Start()
     {
-        
+        ChoseWord();
     }
 
     void ChoseWord()
     {
         randomIndex = Random.Range(0, wordList.Length);
         word = wordList[randomIndex];
+
+        displaySprite.GetComponent<Image>().sprite = objects[randomIndex];
+        for (int i= 0; i< word.Length; i++) 
+        {
+            emptyWord += "-"; 
+        }
+        displayText.SetText(emptyWord);
     }
 }
