98331897f8962dd692f5a80e671c9268b3542667
chourouk07 <117457141+chourouk07@users.noreply.github.com>
2023-05-16 01:43:04 +0100

    Update GameManager.cs
    commitGameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -36,6 +36,7 @@ public class GameManager : MonoBehaviour
         CoolDownTimer();
         DiesplayTimer();
         DisplayScore();
+        CheckScore();
     }
 
     public void DiesplayTimer()
